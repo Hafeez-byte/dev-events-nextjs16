02d563b99039b6b4e8439aed130163f9ed2758b0
Mohammed Abdul Hafeez <mohammedabdulhafeez3112@gmail.com>
2026-07-04 19:59:20 +0530

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,68 @@
+export type EventItem = {
+    image: string;
+    title: string;
+    slug: string;
+    location: string;
+    date: string;
+    time: string;
+};
+
+export const events: EventItem[] = [
+    {
+        image: "/images/event1.png",
+        title: "React Summit US 2025",
+        slug: "react-summit-us-2025",
+        location: "San Francisco, CA, USA",
+        date: "2025-11-07",
+        time: "09:00 AM",
+    },
+    {
+        image: "/images/event2.png",
+        title: "Google I/O Extended Bengaluru",
+        slug: "google-io-extended-bengaluru",
+        location: "Bengaluru, India",
+        date: "2025-08-18",
+        time: "10:00 AM",
+    },
+    {
+        image: "/images/event3.png",
+        title: "Microsoft Build Community Day",
+        slug: "microsoft-build-community-day",
+        location: "Seattle, WA, USA",
+        date: "2025-09-05",
+        time: "09:30 AM",
+    },
+    {
+        image: "/images/event4.png",
+        title: "AWS Community Summit",
+        slug: "aws-community-summit",
+        location: "Singapore",
+        date: "2025-10-12",
+        time: "10:00 AM",
+    },
+    {
+        image: "/images/event5.png",
+        title: "DevOps World Conference",
+        slug: "devops-world-conference",
+        location: "London, UK",
+        date: "2025-09-25",
+        time: "08:30 AM",
+    },
+    {
+        image: "/images/event6.png",
+        title: "Hack the Future 2025",
+        slug: "hack-the-future-2025",
+        location: "Hyderabad, India",
+        date: "2025-11-15",
+        time: "09:00 AM",
+    },
+    {
+        image: "/images/event-full.png",
+        title: "PyCon India",
+        slug: "pycon-india-2025",
+        location: "Pune, India",
+        date: "2025-09-20",
+        time: "09:00 AM",
+    },
+
+];
